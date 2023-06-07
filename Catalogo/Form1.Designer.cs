c8c914e470c79a91c8114ad7a84415e279f5773a
Johnniew <johnniew_@hotmail.com>
2023-06-07 23:41:55 +0200

    parte de archivos casa

---
+++ Catalogo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Catalogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtarchivo = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnexaminar = new System.Windows.Forms.Button();
+            this.btncambiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtarchivo
+            // 
+            this.txtarchivo.Location = new System.Drawing.Point(12, 12);
+            this.txtarchivo.Name = "txtarchivo";
+            this.txtarchivo.Size = new System.Drawing.Size(240, 20);
+            this.txtarchivo.TabIndex = 0;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(12, 38);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(240, 20);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // btnexaminar
+            // 
+            this.btnexaminar.Location = new System.Drawing.Point(258, 12);
+            this.btnexaminar.Name = "btnexaminar";
+            this.btnexaminar.Size = new System.Drawing.Size(105, 20);
+            this.btnexaminar.TabIndex = 2;
+            this.btnexaminar.Text = "Examinar";
+            this.btnexaminar.UseVisualStyleBackColor = true;
+            this.btnexaminar.Click += new System.EventHandler(this.btnexaminar_Click);
+            // 
+            // btncambiar
+            // 
+            this.btncambiar.Location = new System.Drawing.Point(258, 38);
+            this.btncambiar.Name = "btncambiar";
+            this.btncambiar.Size = new System.Drawing.Size(105, 20);
+            this.btncambiar.TabIndex = 3;
+            this.btncambiar.Text = "Examinar";
+            this.btncambiar.UseVisualStyleBackColor = true;
+            this.btncambiar.Click += new System.EventHandler(this.btncambiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 68);
+            this.Controls.Add(this.btncambiar);
+            this.Controls.Add(this.btnexaminar);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtarchivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtarchivo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btnexaminar;
+        private System.Windows.Forms.Button btncambiar;
+    }
+}
+
---
+++ Catalogo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Catalogo
+{
+    public partial class Form1 : Form
+    {
+        string ruta1,ruta2;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btncambiar_Click(object sender, EventArgs e)
+        {
+
+            ruta1 = txtarchivo.Text.ToString();
+            ruta2 += txtnombre.Text.ToString();
+
+            if (File.Exists(ruta2))
+                    File.Delete(ruta2);
+                File.Move(ruta1, ruta2);
+                Console.WriteLine("{0} fue movido a {1}.", ruta1, ruta2);
+                if (File.Exists(ruta1))
+                {
+                    Console.WriteLine("El archivo original sigue existiendo.");
+                }
+                else
+                {
+                    Console.WriteLine("El archivo original ya no existe.");
+                }
+            
+        }
+
+        private void btnexaminar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog catalogo = new OpenFileDialog();
+
+            if (catalogo.ShowDialog() == DialogResult.OK) { 
+                txtarchivo.Text = catalogo.FileName;
+                ruta2 = Path.GetDirectoryName(catalogo.FileName);
+            }
+        }
+    }
+}
